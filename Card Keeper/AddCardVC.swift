b3d90f9df48eae872f4bd84576225713812a0fba
Alex Test Dev <alyskovich@gmail.com>
2017-01-18 00:15:31 -0800

    1

--- Card Keeper/AddCardVC.swift
+++ Card Keeper/AddCardVC.swift
@@ -8,15 +8,42 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class AddCardVC: UIViewController {
+    @IBOutlet weak var testBttn: UIButton!
+    
+    let managedObjectContext = (UIApplication.shared.delegate
+        as! AppDelegate).persistentContainer.viewContext
     
     @IBOutlet weak var saveCardBttn: UIButton!
     @IBOutlet weak var cardNumberField: UITextField!
     
+    @IBAction func testPrint(_ sender: UIButton) {
+        
+    }
+    
     @IBAction func SaveNow(_ sender: UIButton) {
-        print("Tapped!!");
-        print(cardNumberField.text as Any);
+        print("Saving: " + cardNumberField.text! as Any);
+        let entityDescription =
+            NSEntityDescription.entity(forEntityName: "Cards",
+                                       in: managedObjectContext)
+        
+        let cardDetails = Cards(entity: entityDescription!,
+                               insertInto: managedObjectContext)
+        
+        cardDetails.cardNumber = Int64(cardNumberField.text!)!
+        
+        
+        do {
+            try managedObjectContext.save()
+            //status.text = "Contact Saved"
+            
+        } catch let error {
+            //status.text = error.localizedDescription
+            print(error.localizedDescription)
+        }
+
     }
     
 }
