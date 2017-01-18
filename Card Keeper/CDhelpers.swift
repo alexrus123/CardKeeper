202865ffaaef28e62c1167a900d9da51b24857eb
Alex Test Dev <alyskovich@gmail.com>
2017-01-17 23:39:39 -0800

    prep

---
+++ Card Keeper/CDhelpers.swift
@@ -0,0 +1,13 @@
+//
+//  CDhelpers.swift
+//  Card Keeper
+//
+//  Created by Aliaksei Lyskovich on 1/17/17.
+//  Copyright © 2017 Aliaksei Lyskovich. All rights reserved.
+//
+
+import Foundation
+
+class CDhelper {
+    
+}
