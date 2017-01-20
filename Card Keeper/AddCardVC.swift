7609c893ad9219b58941bbef74db35b66a8f6ed4
Alex Test Dev <alyskovich@gmail.com>
2017-01-19 22:50:44 -0800

    kd

--- Card Keeper/AddCardVC.swift
+++ Card Keeper/AddCardVC.swift
@@ -13,49 +13,16 @@ import CoreData
 class AddCardVC: UIViewController {
     @IBOutlet weak var testBttn: UIButton!
     
-    let managedObjectContext = (UIApplication.shared.delegate
-        as! AppDelegate).persistentContainer.viewContext
-    
     @IBOutlet weak var saveCardBttn: UIButton!
     @IBOutlet weak var cardNumberField: UITextField!
     
     @IBAction func testPrint(_ sender: UIButton) {
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Cards")
         
-        do {
-            let results = try managedObjectContext.fetch(fetchRequest)
-            let  Locations = results as! [Cards]
-            
-            for location in Locations {
-                print(String(location.cardNumber))
-            }
-        } catch let error as NSError {
-            //print("Could not fetch \(error)”),
-            print(error.code)
-        }
     }
     
     @IBAction func SaveNow(_ sender: UIButton) {
-        print("Saving: " + cardNumberField.text! as Any);
-        let entityDescription =
-            NSEntityDescription.entity(forEntityName: "Cards",
-                                       in: managedObjectContext)
-        
-        let cardDetails = Cards(entity: entityDescription!,
-                               insertInto: managedObjectContext)
-        
-        cardDetails.cardNumber = Int64(cardNumberField.text!)!
-        
-        
-        do {
-            try managedObjectContext.save()
-            //status.text = "Contact Saved"
-            
-        } catch let error {
-            //status.text = error.localizedDescription
-            print(error.localizedDescription)
-        }
-
+        print("Saving: " + cardNumberField.text! as Any)
+        CDhelper.saveToCoreData(cardNumberVal: Int64(cardNumberField.text!)!)
     }
     
 }
--- Card Keeper/CDhelpers.swift
+++ Card Keeper/CDhelpers.swift
@@ -7,7 +7,50 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
 
 class CDhelper {
+    
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    func fetchCoreData(){
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Cards")
+        
+        do {
+            let results = try managedObjectContext.fetch(fetchRequest)
+            let  Locations = results as! [Cards]
+            
+            for location in Locations {
+                print(String(location.cardNumber))
+            }
+        } catch let error as NSError {
+            //print("Could not fetch \(error)”),
+            print(error.code)
+        }
+    }
+    
+    func saveToCoreData(cardNumberVal: Int64){
+        let entityDescription =
+            NSEntityDescription.entity(forEntityName: "Cards",
+                                       in: managedObjectContext)
+        
+        let cardDetails = Cards(entity: entityDescription!,
+                                insertInto: managedObjectContext)
+        
+        cardDetails.cardNumber = cardNumberVal
+
+        //cardDetails.cardNumber = Int64(cardNumberField.text!)!
+        
+        
+        do {
+            try managedObjectContext.save()
+            //status.text = "Contact Saved"
+            
+        } catch let error {
+            //status.text = error.localizedDescription
+            print(error.localizedDescription)
+        }
+    }
     
 }
