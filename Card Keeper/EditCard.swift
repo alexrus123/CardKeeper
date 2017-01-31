d0b5e09c2f8decd03138248c3cf0456c5fba54c7
Alex Test Dev <alyskovich@gmail.com>
2017-01-31 10:38:45 -0800

    9

--- Card Keeper/EditCard.swift
+++ Card Keeper/EditCard.swift
@@ -10,5 +10,19 @@ import Foundation
 import UIKit
 
 class EditCard: UIViewController{
+    @IBOutlet weak var saveChangesBttn: UIButton!
+    @IBOutlet weak var selectedCardDescription: UITextField!
+    @IBOutlet weak var selectedCardNumberField: UITextField!
+    @IBOutlet weak var selectedCardBackImage: UIImageView!
+    var receivedData = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
 }
