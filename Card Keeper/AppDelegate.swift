bc80cb729d04c058394b1227d7cb33c117dc070e
Alex Test Dev <alyskovich@gmail.com>
2017-02-27 00:05:44 -0800

    wip

--- Card Keeper/AppDelegate.swift
+++ Card Keeper/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import waterwheel
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        waterwheel.setDrupalURL("http://kids.qatsys.com")
+        waterwheel.nodeGet(nodeId: "11", params: nil) { (success, response, json, error) in
+            print(json)
+        }
         return true
     }
 
--- Card Keeper/SettingsView.swift
+++ Card Keeper/SettingsView.swift
@@ -15,11 +15,58 @@ class Settings : UIViewController{
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginBttn: UIButton!
+    let ww = waterwheelLoginViewController()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ww.usernameField.text = "demo"
+        ww.passwordField.text = "demo"
+    
+    // Lets add our function that will be run when the request is completed.
+    ww.loginRequestCompleted = { (success, error) in
+    if (success) {
+    // Do something related to a successfull login
+    print("successfull login")
+    self.dismiss(animated: true, completion: nil)
+    } else {
+    print (error!)
+    }
     }
     
+    // Define our logout action
+    ww.logoutRequestCompleted = { (success, error) in
+    if (success) {
+    print("successfull logout")
+    // Do something related to a successfull logout
+    self.dismiss(animated: true, completion: nil)
+    } else {
+    print (error!)
+    }
+    }
+    
+    // Define our cancel button action
+    ww.cancelButtonHit = {
+    self.dismiss(animated: true, completion: nil)
+    }
+    
+        /*
+    // Do any additional setup after loading the view, typically from a nib.
+    loginBttn.isTouchInside = {
+    // Lets Present our Login View Controller since this closure is for the loginButton press
+    
+    }
+    
+    loginBttn.touch = { (success, error) in
+    print("logged out")
+    }
+    */
+    }
+    
+    @IBAction func loginNow(_ sender: Any) {
+        self.present(ww, animated: true, completion: nil)
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
