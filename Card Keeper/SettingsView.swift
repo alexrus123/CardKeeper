cbf93bfee56f894c2e63c66512baabe276b3c7ff
Alex Test Dev <alyskovich@gmail.com>
2017-02-26 23:38:00 -0800

    wip

--- Card Keeper/SettingsView.swift
+++ Card Keeper/SettingsView.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+class Setting : UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
